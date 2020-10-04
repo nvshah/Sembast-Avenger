94919cb38dc9b7baa31d277a705026e12fa3dc2a
nvshah <nipunshah6776@gmail.com>
2020-10-04 22:08:42 +0530

    minor bug fix - added bloc-provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import './view/home_page.dart';
+import './data/bloc.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,15 +12,19 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-        accentColor: Colors.amber,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    // Wrapping the whole app with BlocProvider to get access to FruitBloc everywhere
+    // BlocProvider extends InheritedWidget.
+    return BlocProvider(
+      bloc: AvengerBloc(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.red,
+          accentColor: Colors.amber,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: HomePage(),
       ),
-      home: HomePage(),
     );
   }
 }
-
